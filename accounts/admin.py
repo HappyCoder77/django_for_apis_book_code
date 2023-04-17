fac853d6d6e597d429f187afcbc7e6c762dc7de9
Saúl F. Rojas G <saul_rr77@hotmail.com>
2023-04-17 12:55:41 -0400

    blog API setup

---
+++ accounts/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import CustomUserCreationForm, CustomuserChangeForm
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomuserChangeForm
+    model = CustomUser
+    list_display = [
+        "email",
+        "username",
+        "name",
+        "is_staff",
+    ]
+
+    fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("name",)}),)
+    add_fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("name",)}),)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
